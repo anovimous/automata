89f6bbf867a89cf0e46e65dc14c42540df741e98
anovimous <anovimous@gmail.com>
2025-12-22 22:12:24 +0200

    feat: initial implementation of ResponseBodyProperty entity

---
+++ code/automata/src/main/java/com/automata/Response/ResponseBodyProperty.java
@@ -0,0 +1,38 @@
+package com.automata.Response;
+
+import com.automata.host.Host;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class ResponseBodyProperty {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false)
+	private String property;
+	
+	private int parentId;
+	
+	@Column(nullable = false)
+	@ManyToOne
+	private Response response;
+	
+	@Column(nullable = false)
+	@ManyToOne
+	private Host host;
+	
+}
