527300a67f620ff84d00347628620e9d25d07f24
anovimous <anovimous@gmail.com>
2025-12-22 22:09:40 +0200

    feat: initial implementation of Tenant entity

---
+++ code/automata/src/main/java/com/automata/request/tenant/Tenant.java
@@ -0,0 +1,36 @@
+package com.automata.request.tenant;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class Tenant {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@CreationTimestamp
+	@Column(nullable = false, updatable = false)
+	private LocalDate creationDate;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private String email;
+	
+}
