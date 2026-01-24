a2b25122f1debd2d40f194d55edeb34ce03c75e4
anovimous <anovimous@gmail.com>
2026-01-24 15:53:00 +0200

    feat(host): created HostUtils class

---
+++ code/automata/src/main/java/com/automata/host/HostUtils.java
@@ -0,0 +1,25 @@
+package com.automata.host;
+
+import org.apache.commons.validator.routines.DomainValidator;
+import org.apache.commons.validator.routines.InetAddressValidator;
+
+import com.automata.common.utils.ValidationResult;
+
+public abstract class HostUtils {
+
+	public static int identifyLevel(String host) {
+		return host.split("\\.").length;
+
+	}
+
+	public static ValidationResult validateHostFormat(String host) {
+		InetAddressValidator ipValidator = InetAddressValidator.getInstance();
+		DomainValidator domainValidator = DomainValidator.getInstance(true); // allow TLDs
+		return ipValidator.isValid(host) || domainValidator.isValid(host) ? ValidationResult.valid() : ValidationResult.invalid();
+	}
+	
+	public static ValidationResult validateRateLimits(int hostLimit, int longLimit, int shortLimit) {
+		return hostLimit >= (longLimit + shortLimit) ? ValidationResult.valid() : ValidationResult.invalid();
+	}
+
+}
