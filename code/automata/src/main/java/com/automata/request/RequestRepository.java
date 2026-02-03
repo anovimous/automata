66002362076ae353060457a02adc90477cdbe651
anovimous <anovimous@gmail.com>
2026-02-03 21:55:56 +0200

    feat(request): created RequestRepository including one method

---
+++ code/automata/src/main/java/com/automata/request/RequestRepository.java
@@ -0,0 +1,12 @@
+package com.automata.request;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long>, JpaSpecificationExecutor<Request> {
+
+	boolean existsByIdAndEqualitySetIsNotEmpty(Long requestId);
+
+}
