3c7b443361f1987d13d9ee9db68a7374a06404a5
anovimous <anovimous@gmail.com>
2026-02-03 22:06:28 +0200

    feat(request): removed relation to QueryParameterValue entity

--- code/automata/src/main/java/com/automata/request/parameter/QueryParameter.java
+++ code/automata/src/main/java/com/automata/request/parameter/QueryParameter.java
@@ -1,9 +1,7 @@
 package com.automata.request.parameter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.automata.request.Request;
+import com.automata.request.valueanalysis.ValueAnalysis;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,30 +9,39 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@RequiredArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class QueryParameter {
 
+	public static QueryParameter of(String parameter, String value) {
+		QueryParameter queryParam = new QueryParameter();
+		queryParam.setParameter(parameter);
+		queryParam.setValue(value);
+		return queryParam;
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	
+	private Long id;
+
 	@Column(nullable = false)
 	private String parameter;
-	
+
+	@Column(nullable = false)
+	private String value;
+
 	@Column(nullable = false)
 	@ManyToOne
 	private Request request;
-	
-	@OneToMany(mappedBy = "queryParameter")
-	private List<QueryParameterValue> queryParameterValues = new ArrayList<>();
-	
-	
+
+	@OneToOne
+	private ValueAnalysis valueAnalysis;
+
 }
