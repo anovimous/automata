e5ea6338f302bd149840325f65689e1051d65423
anovimous <anovimous@gmail.com>
2026-02-03 21:52:49 +0200

    feat(request): added relation to Program entity and did some formatting

--- code/automata/src/main/java/com/automata/request/Request.java
+++ code/automata/src/main/java/com/automata/request/Request.java
@@ -5,23 +5,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.lang.Nullable;
 
 import com.automata.host.Host;
+import com.automata.program.Program;
 import com.automata.request.body.BodyProperty;
-import com.automata.request.enums.ContentType;
-import com.automata.request.enums.Method;
-import com.automata.request.enums.Source;
+import com.automata.request.common.enums.ContentType;
+import com.automata.request.common.enums.Method;
+import com.automata.request.common.enums.Source;
+import com.automata.request.equalityset.RequestEqualitySet;
 import com.automata.request.parameter.QueryParameter;
 import com.automata.request.path.PathVariable;
-import com.automata.request.tenant.Tenant;
+import com.automata.tenant.Tenant;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
@@ -36,48 +41,57 @@ public class Request {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	
+	private Long id;
+
 	@CreationTimestamp
 	@Column(nullable = false, updatable = false)
 	private LocalDate insertionDate;
-	
-	@Column(nullable = false)
-	@Enumerated(EnumType.STRING)
-	private Source source;
-	
+
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private Method method;
-	
+
 	@Column(nullable = false)
 	private String computatedPath;
-	
+
+	@Nullable
 	private String extension;
-	
-	@OneToMany(mappedBy = "request")
-	private List<PathVariable> pathVariables = new ArrayList<>();
-	
-	private Integer numberOfProperties;
-	
+
+	// This is automatically set, equals bodyProperties.length()
+	private int numberOfProperties;
+
 	// For now, request will only support JSON and POST form data
-	
+
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private ContentType contentType;
-	
-	@OneToMany(mappedBy = "request")
-	private List<BodyProperty> bodyProperties = new ArrayList<>();
-	
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Source source;
+
+	@Column(nullable = false)
 	@ManyToOne
-	private Tenant tenant;
-	
-	@OneToMany(mappedBy = "request")
-	private List<QueryParameter> parameters = new ArrayList<>();
-	
+	private Program program;
+
 	@Column(nullable = false)
 	@ManyToOne
 	private Host host;
-	
-	
+
+	@Nullable
+	@ManyToOne
+	private Tenant tenant;
+
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "request")
+	private List<PathVariable> pathVariables = new ArrayList<>();
+
+	@OneToMany(mappedBy = "request")
+	private List<BodyProperty> bodyProperties = new ArrayList<>();
+
+	@OneToMany(mappedBy = "request")
+	private List<QueryParameter> parameters = new ArrayList<>();
+
+	@ManyToMany
+	private List<RequestEqualitySet> equalitySet = new ArrayList<>();
+
 }
