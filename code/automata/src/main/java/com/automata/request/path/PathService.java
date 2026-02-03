971b181e1f4b1d6e64d9b52978d2859c3f3f2290
anovimous <anovimous@gmail.com>
2026-02-03 22:05:18 +0200

    feat(request): created PathService including one method

---
+++ code/automata/src/main/java/com/automata/request/path/PathService.java
@@ -0,0 +1,43 @@
+package com.automata.request.path;
+
+import java.util.stream.Collectors;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.automata.request.Request;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PathService {
+
+	private final PathVariableRepository pathVariableRepo;
+
+	@Transactional
+	public void updatePath(Request request, String path) {
+
+		PathUtils.validatePath(path).ifNotValidThrow(() -> new IllegalArgumentException("Path format invalid"));
+
+		PathParsingResult parseResult = PathUtils.parsePath(path);
+
+		List<Long> toDeletePathVariables = request.getPathVariables().stream().map(PathVariable::getId)
+				.collect(Collectors.toList());
+
+		pathVariableRepo.deleteAllByIdInBatch(toDeletePathVariables);
+
+		request.setComputatedPath(parseResult.getComputatedPath());
+
+		request.setExtension(parseResult.getExtension());
+
+		List<PathVariable> toSetPathVariables = parseResult.getPathVariables();
+
+		pathVariableRepo.saveAll(toSetPathVariables);
+
+		request.setPathVariables(toSetPathVariables);
+
+	}
+
+}
