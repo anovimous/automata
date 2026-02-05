ec8f39e1c941b03372f03d99981ec767b1831f09
anovimous <anovimous@gmail.com>
2026-02-05 17:13:21 +0200

    refactor(request): updated composite pk config in Header

--- code/automata/src/main/java/com/automata/request/header/HeaderCompositePK.java
+++ code/automata/src/main/java/com/automata/request/header/HeaderCompositePK.java
@@ -1,23 +1,37 @@
 package com.automata.request.header;
 
 import java.io.Serializable;
-
-import com.automata.host.Host;
+import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 @Embeddable
-public class HeaderCompositePK implements Serializable{
+@Getter
+@Setter
+public class HeaderCompositePK implements Serializable {
 
 	private static final long serialVersionUID = 6699915325160645145L;
-	
+
 	@Column(nullable = false)
 	private String header;
-	
-	@ManyToOne
-	@Column(nullable = false)
-	private Host host;
 
+	private Long hostId;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof HeaderCompositePK))
+			return false;
+		HeaderCompositePK that = (HeaderCompositePK) o;
+		return Objects.equals(header, that.header) && Objects.equals(hostId, that.hostId);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(header, hostId);
+	}
 }
