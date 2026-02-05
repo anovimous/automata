f3e8ca092b288080a63de86de6234a7c8097b5a2
anovimous <anovimous@gmail.com>
2026-02-05 17:33:01 +0200

    feat(request): implemented operation addRawRequest in service class

--- code/automata/src/main/java/com/automata/request/RequestService.java
+++ code/automata/src/main/java/com/automata/request/RequestService.java
@@ -1,20 +1,29 @@
 package com.automata.request;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.automata.common.utils.Base64Utils;
+import com.automata.host.Host;
+import com.automata.host.HostRepository;
 import com.automata.host.common.dto.RequestFilter;
-import com.automata.job.JobRepository;
+import com.automata.job.RunJobRepository;
+import com.automata.request.body.BodyProperty;
 import com.automata.request.body.BodyPropertyService;
+import com.automata.request.common.dto.InternalRequestPersistanceDto;
 import com.automata.request.common.dto.RawRequestAdditionDto;
 import com.automata.request.common.dto.RequestAdditionDto;
 import com.automata.request.common.dto.RequestPatchDto;
 import com.automata.request.equalityset.RequestEqualitySetRepository;
+import com.automata.request.header.HeaderService;
+import com.automata.request.parameter.QueryParameter;
 import com.automata.request.parameter.QueryParameterService;
 import com.automata.request.path.PathService;
+import com.automata.request.path.PathVariable;
 import com.automata.tenant.Tenant;
 import com.automata.tenant.TenantRepository;
 
@@ -31,13 +40,17 @@ public class RequestService {
 
 	private final TenantRepository tenantRepo;
 
-	private final JobRepository jobRepo;
+	private final RunJobRepository jobRepo;
+
+	private final HostRepository hostRepo;
 
 	private final PathService pathService;
 
 	private final QueryParameterService queryParameterService;
 
 	private final BodyPropertyService bodyService;
+
+	private final HeaderService headerService;
 
 	public Request getRequestById(Long requestId) {
 
@@ -56,18 +69,43 @@ public class RequestService {
 
 	}
 
-	public Request addRequest(RequestAdditionDto dto) {
+	public Request addRequestViaComponents(RequestAdditionDto dto) {
 
-		// add request to its own equality set if added with comparator but no equality
-		// with something found
 		return null;
+
 	}
 
 	public Request addRawRequest(RawRequestAdditionDto dto) {
 
 		// add request to its own equality set if added with comparator but no equality
 		// with something found
-		return null;
+
+		Host host = hostRepo.findById(dto.hostId()).orElseThrow(() -> new EntityNotFoundException("Host not found"));
+
+		Tenant tenant = tenantRepo.findById(dto.tenantId()).orElse(null);
+
+		String rawRequest = Base64Utils.decode(dto.requestBase64());
+
+		RequestParseResult parseResult = RequestUtils.parseRawRequest(rawRequest);
+
+		parseResult.getValidationResult()
+				.ifNotValidThrow(() -> new IllegalArgumentException(parseResult.getValidationResult().getMessage()));
+
+		headerService.addHeaders(parseResult.getHeaders(), host);
+
+		InternalRequestPersistanceDto internalDto = InternalRequestPersistanceDto.builder().source(dto.source())
+				.method(parseResult.getMethod()).version(parseResult.getVersion())
+				.extension(parseResult.getPathParseResult().getExtension()).contentType(parseResult.getContentType())
+				.requestParseResult(parseResult).build();
+
+		Request persistedRequest = this.internalPersistRequest(internalDto);
+
+		persistedRequest.setHost(host);
+
+		persistedRequest.setTenant(tenant);
+
+		return persistedRequest;
+
 	}
 
 	public Request patchRequest(Long requestId, RequestPatchDto dto) {
@@ -102,8 +140,7 @@ public class RequestService {
 				.updateQueryParametersProvidedQueryString(request, Base64Utils.decode(queryStringBase64)));
 
 		if (dto.bodyBase64().isPresent() && dto.contentType().isPresent())
-			bodyService.updateBodyProvidedRaw(request, dto.contentType().get(),
-					Base64Utils.decode(dto.bodyBase64().get()));
+			bodyService.updateBodyProvidedRaw(request, Base64Utils.decode(dto.bodyBase64().get()));
 		else {
 			if (dto.bodyBase64().isPresent() || dto.contentType().isPresent())
 				throw new IllegalArgumentException(
@@ -117,6 +154,43 @@ public class RequestService {
 	public void deleteRequest(Long requestId) {
 		// check if any unfinished job is tied to it
 		// remove equality set if it becomes empty
+
+	}
+
+	private Request internalPersistRequest(InternalRequestPersistanceDto internalDto) {
+
+		Request request = new Request();
+
+		request.setSource(internalDto.getSource());
+
+		request.setMethod(internalDto.getMethod());
+
+		request.setVersion(internalDto.getVersion());
+
+		request.setContentType(internalDto.getContentType());
+
+		request.setComputatedPath(internalDto.getRequestParseResult().getPathParseResult().getComputatedPath());
+
+		request.setExtension(internalDto.getRequestParseResult().getPathParseResult().getExtension());
+
+		List<PathVariable> persistedPathVariables = pathService
+				.persistPathVariables(internalDto.getRequestParseResult().getPathParseResult().getPathVariables());
+
+		List<QueryParameter> persistedQueryParameters = queryParameterService.persistQueryParameters(
+				internalDto.getRequestParseResult().getQueryStringParseResult().getQueryParameters());
+
+		List<BodyProperty> persistedBodyProperties = bodyService
+				.persistBodyProperties(internalDto.getRequestParseResult().getBodyParseResult().getBodyProperties());
+
+		request.setPathVariables(persistedPathVariables);
+
+		request.setParameters(persistedQueryParameters);
+
+		request.setBodyProperties(persistedBodyProperties);
+
+		requestRepo.save(request);
+
+		return request;
 
 	}
 
