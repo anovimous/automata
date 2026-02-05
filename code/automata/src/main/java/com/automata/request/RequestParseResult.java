ae9de6a9e4f18b78ec049e5e300437a97072e2c2
anovimous <anovimous@gmail.com>
2026-02-05 17:29:22 +0200

    feat(request): created RequestParseResult class

---
+++ code/automata/src/main/java/com/automata/request/RequestParseResult.java
@@ -0,0 +1,39 @@
+package com.automata.request;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.automata.common.utils.ValidationResult;
+import com.automata.request.body.BodyParseResult;
+import com.automata.request.common.enums.ContentType;
+import com.automata.request.common.enums.Method;
+import com.automata.request.header.Header;
+import com.automata.request.parameter.QueryStringParseResult;
+import com.automata.request.path.PathParsingResult;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class RequestParseResult {
+
+	private Method method;
+
+	private String version;
+
+	private ContentType contentType;
+
+	private PathParsingResult pathParseResult;
+
+	private QueryStringParseResult queryStringParseResult;
+
+	private BodyParseResult bodyParseResult;
+
+	@Builder.Default
+	private List<Header> headers = new ArrayList<>();
+
+	private ValidationResult validationResult;
+}
