52151d6b222f35ab1105ffc5ac3bc8983f8455f8
anovimous <anovimous@gmail.com>
2026-02-09 20:35:56 +0200

    feat(request): added relation to Host and Request

--- code/automata/src/main/java/com/automata/request/equalityset/RequestEqualitySet.java
+++ code/automata/src/main/java/com/automata/request/equalityset/RequestEqualitySet.java
@@ -1,11 +1,17 @@
 package com.automata.request.equalityset;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.automata.host.Host;
+import com.automata.request.Request;
 import com.automata.request.comparator.Comparator;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +26,16 @@ public class RequestEqualitySet {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	private Long currentNumberOfRequests;
-	
-	@ManyToOne
+
+	private Integer currentNumberOfRequests;
+
+	@ManyToOne(optional = false)
 	private Comparator comparator;
-	
+
+	@ManyToOne(optional = false)
+	public Host host;
+
+	@ManyToMany
+	private Set<Request> requests = new HashSet<>();
+
 }
