25c82b720df54bf75b0e93e941485bc8a2967d57
anovimous <anovimous@gmail.com>
2026-02-09 20:29:51 +0200

    feat: fully implemented mapping functionalities

--- code/automata/src/main/java/com/automata/request/RequestMapper.java
+++ code/automata/src/main/java/com/automata/request/RequestMapper.java
@@ -3,17 +3,28 @@ package com.automata.request;
 import org.springframework.data.domain.Page;
 
 import com.automata.request.common.dto.RequestResponse;
+import com.automata.request.common.dto.RequestResponse.RequestResponseBuilder;
 
 public abstract class RequestMapper {
 
 	public static Page<RequestResponse> toRequestResponseList(Page<Request> requests) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return requests.map(RequestMapper::toRequestResponse);
+
 	}
 
 	public static RequestResponse toRequestResponse(Request request) {
-		// TODO Auto-generated method stub
-		return null;
+
+		RequestResponseBuilder builder = RequestResponse.builder();
+
+		builder.method(request.getMethod()).computatedPath(request.getComputatedPath())
+				.extension(request.getExtension()).version(request.getVersion())
+				.numberOfProperties(request.getNumberOfProperties()).contentType(request.getContentType())
+				.source(request.getSource()).hostId(request.getHost().getId()).tenantId(request.getTenant().getId())
+				.programId(request.getProgram().getId());
+
+		return builder.build();
+
 	}
 
 }
