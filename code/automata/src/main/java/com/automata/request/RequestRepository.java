709b9b8d60793bf7102f7930c99989f484920ea3
anovimous <anovimous@gmail.com>
2026-02-09 20:34:27 +0200

    feat(request): added new methods to RequestRepository

--- code/automata/src/main/java/com/automata/request/RequestRepository.java
+++ code/automata/src/main/java/com/automata/request/RequestRepository.java
@@ -1,12 +1,26 @@
 package com.automata.request;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+
+import com.automata.host.Host;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long>, JpaSpecificationExecutor<Request> {
 
 	boolean existsByIdAndEqualitySetIsNotEmpty(Long requestId);
+
+	int countByHost(Host host);
+
+	List<Long> findIdsByHost(Host host);
+
+	Page<Request> findByHost(Host host, Pageable ofSize);
+
+	Page<Request> findByIds(List<Long> requestsIds, Pageable ofSize);
 
 }
