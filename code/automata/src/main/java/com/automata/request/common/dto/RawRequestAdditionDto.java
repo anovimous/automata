0c574274e739b948a905fc10a41caf73a53aea07
anovimous <anovimous@gmail.com>
2026-02-10 20:55:32 +0200

    feat: added field for base64 response

--- code/automata/src/main/java/com/automata/request/common/dto/RawRequestAdditionDto.java
+++ code/automata/src/main/java/com/automata/request/common/dto/RawRequestAdditionDto.java
@@ -2,6 +2,6 @@ package com.automata.request.common.dto;
 
 import com.automata.request.common.enums.Source;
 
-public record RawRequestAdditionDto(String requestBase64, Source source, Long hostId, Long tenantId) {
+public record RawRequestAdditionDto(String requestBase64, Source source, Long hostId, Long tenantId,String responseBase64) {
 
 }
