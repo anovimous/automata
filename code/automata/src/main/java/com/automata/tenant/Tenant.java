5e4105b69a615450151f4d729befc7f4e9e573f3
anovimous <anovimous@gmail.com>
2026-02-11 10:26:22 +0200

    feat(tenant): added relation to host

--- code/automata/src/main/java/com/automata/tenant/Tenant.java
+++ code/automata/src/main/java/com/automata/tenant/Tenant.java
@@ -4,33 +4,43 @@ import java.time.LocalDate;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.automata.host.Host;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 @Setter
 public class Tenant {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	
+	private Long id;
+
 	@CreationTimestamp
 	@Column(nullable = false, updatable = false)
 	private LocalDate creationDate;
-	
+
 	@Column(nullable = false)
 	private String name;
-	
+
 	@Column(nullable = false)
 	private String email;
-	
+
+	@ManyToOne(optional = false)
+	private Host host;
+
 }
