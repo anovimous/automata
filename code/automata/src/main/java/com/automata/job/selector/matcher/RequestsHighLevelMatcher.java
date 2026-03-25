ffa9f25a498efa09450deb12cb04cd3356f33dd6
anovimous <anovimous@gmail.com>
2026-03-25 17:05:23 +0200

    feat(job): created RequestsHighLevelMatcher class for high level matching of requests during target selection op

---
+++ code/automata/src/main/java/com/automata/job/selector/matcher/RequestsHighLevelMatcher.java
@@ -0,0 +1,14 @@
+package com.automata.job.selector.matcher;
+
+import java.util.Optional;
+import java.util.Set;
+
+public class RequestsHighLevelMatcher {
+
+	private Long hostId;
+
+	private Optional<Set<Long>> tenantsIds;
+
+	private Optional<Set<Long>> equalitySetsIds;
+
+}
