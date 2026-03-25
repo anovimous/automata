9c5ffb91d086a2bab3da9a1dc270619bc2302d56
anovimous <anovimous@gmail.com>
2026-03-25 19:36:58 +0200

    refactor(request): added all args constructor lombok annotation

--- code/automata/src/main/java/com/automata/request/body/BodyParseResult.java
+++ code/automata/src/main/java/com/automata/request/body/BodyParseResult.java
@@ -3,11 +3,13 @@ package com.automata.request.body;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Setter
 @Getter
 public class BodyParseResult {
