03e3152e506516a1452f0014509469049a92f352
anovimous <anovimous@gmail.com>
2026-03-25 17:38:47 +0200

    feat: added property rawValue to enum ContentType

--- code/automata/src/main/java/com/automata/request/common/enums/ContentType.java
+++ code/automata/src/main/java/com/automata/request/common/enums/ContentType.java
@@ -1,5 +1,16 @@
 package com.automata.request.common.enums;
 
 public enum ContentType {
-JSON, MULTIPART, FORM, UNSUPPORTED
+	JSON("application/json"), MULTIPART("multipart/form-data"), FORM("application/x-www-form-urlencoded"),
+	UNSUPPORTED("unsupported");
+
+	private final String rawValue;
+
+	ContentType(String rawValue) {
+		this.rawValue = rawValue;
+	}
+
+	public String getRaw() {
+		return rawValue;
+	}
 }
