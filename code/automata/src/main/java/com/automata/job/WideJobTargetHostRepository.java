5b8af279d48c9cadfedeff51012f4841a245ab99
anovimous <anovimous@gmail.com>
2026-03-25 17:22:52 +0200

    feat(job): created repos for join entities relative to HttpJob targets

---
+++ code/automata/src/main/java/com/automata/job/WideJobTargetHostRepository.java
@@ -0,0 +1,25 @@
+package com.automata.job;
+
+import java.util.stream.Stream;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.automata.host.common.dto.HostInternalDto;
+
+@Repository
+public interface WideJobTargetHostRepository extends JpaRepository<WideJobTargetHost, Long> {
+
+	@Query(value = """
+			SELECT new com.automata.host.common.dto.HostInternalDto(
+			    h.id,
+			    h.host,
+			    h.scope
+			)
+			FROM WideJobTargetHost h
+			WHERE h.job = :job
+			""", countQuery = "SELECT COUNT(h) FROM WideJobTargetHost h WHERE h.job = :job")
+	Stream<HostInternalDto> getHostDtosByJob(WideHttpJob fullyConfiguredJob);
+
+}
