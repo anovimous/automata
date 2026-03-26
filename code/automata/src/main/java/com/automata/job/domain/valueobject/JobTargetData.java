380e309490602720099511485f3078d318badcea
anovimous <anovimous@gmail.com>
2026-03-26 15:18:43 +0200

    refactor(job): deleted unused valueobject

--- code/automata/src/main/java/com/automata/job/domain/valueobject/JobTargetData.java
+++
@@ -1,9 +0,0 @@
-package com.automata.job.domain.valueobject;
-
-import java.util.List;
-
-import lombok.Builder;
-
-@Builder
-public record JobTargetData(List<RequestData> requests, List<HostData> hosts) {
-}
