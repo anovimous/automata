d72b262f5a7427245cf30f8c2a718d8dee47cdd6
anovimous <anovimous@gmail.com>
2026-03-26 13:24:24 +0200

    feat(job): added some skeleton methods to controller

--- code/automata/src/main/java/com/automata/job/HttpJobController.java
+++ code/automata/src/main/java/com/automata/job/HttpJobController.java
@@ -3,11 +3,14 @@ package com.automata.job;
 import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.automata.common.dto.response.PageHolderResponse;
 import com.automata.job.common.dto.HttpJobCreationRequest;
 import com.automata.job.common.enums.HttpJobScope;
 
@@ -19,6 +22,16 @@ import lombok.RequiredArgsConstructor;
 public class HttpJobController {
 
 	private final HttpJobService jobService;
+
+	@GetMapping("/{jobId}")
+	public ResponseEntity<HttpJobDto> getJob() {
+
+	}
+
+	@GetMapping("")
+	public ResponseEntity<PageHolderResponse<HttpJobDto>> getJobs(@RequestBody HttpJobFilter filter) {
+
+	}
 
 	@PostMapping("")
 	public ResponseEntity<Void> createDraftJob(@RequestBody HttpJobCreationRequest request) {
@@ -37,6 +50,31 @@ public class HttpJobController {
 		};
 
 		return ResponseEntity.created(URI.create(String.format("/api/jobs/http/%d", newlyCreatedJob.getId()))).build();
+
+	}
+
+	@PostMapping("/{draftJobId}/queue")
+	public ResponseEntity<Void> queueJob(@PathVariable Long draftJobId) {
+
+	}
+
+	@PostMapping("/{draftJobId}/schedule")
+	public ResponseEntity<Void> scheduleJob(@PathVariable Long draftJobId, @RequestBody JobScheduleRequest req) {
+
+	}
+
+	@PostMapping("/{jobId}/pause")
+	public ResponseEntity<Void> pauseRunningJob(@PathVariable Long draftJobId) {
+
+	}
+
+	@PostMapping("/{jobId}/resume")
+	public ResponseEntity<Void> resumePausedJob(@PathVariable Long draftJobId) {
+
+	}
+
+	@PostMapping("/{jobId}/cancel")
+	public ResponseEntity<Void> cancelDraftOrPausedJob(@PathVariable Long draftJobId) {
 
 	}
 
