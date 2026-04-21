c06051f1dcbdd60b3169877905528586cacc03c1
anovimous <anovimous@gmail.com>
2026-04-21 19:45:57 +0300

    refactor: removed rebundant configs after the setting of properties in application.properties

--- code/automata/src/main/java/com/automata/GenericRabbitMQConfig.java
+++ code/automata/src/main/java/com/automata/GenericRabbitMQConfig.java
@@ -1,12 +1,9 @@
 package com.automata;
 
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.autoconfigure.amqp.RabbitTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,16 +11,6 @@ import org.springframework.context.annotation.Configuration;
 public class GenericRabbitMQConfig {
 
 	public static final String EXCHANGE = "automataDirectExchange";
-
-	@Bean
-	CachingConnectionFactory connectionFactory() {
-		CachingConnectionFactory factory = new CachingConnectionFactory();
-		factory.setHost("localhost");
-		factory.setPort(5672);
-		factory.setUsername("guest");
-		factory.setPassword("guest");
-		return factory;
-	}
 
 	@Bean
 	DirectExchange exchange() {
@@ -36,10 +23,10 @@ public class GenericRabbitMQConfig {
 	}
 
 	@Bean
-	AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
-		RabbitTemplate template = new RabbitTemplate(connectionFactory);
-		template.setExchange(EXCHANGE);
-		template.setMessageConverter(jsonMessageConverter());
-		return template;
+	RabbitTemplateCustomizer rabbitTemplateCustomizer() {
+		return template -> {
+			template.setExchange(EXCHANGE);
+			template.setMessageConverter(jsonMessageConverter());
+		};
 	}
 }
