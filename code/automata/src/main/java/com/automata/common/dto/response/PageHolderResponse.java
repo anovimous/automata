8261beae4347f80c08395e1d4039688284a382f7
anovimous <anovimous@gmail.com>
2026-04-21 22:35:37 +0300

    refactor: added @Getter annotation

--- code/automata/src/main/java/com/automata/common/dto/response/PageHolderResponse.java
+++ code/automata/src/main/java/com/automata/common/dto/response/PageHolderResponse.java
@@ -4,23 +4,25 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 
-public class PageHolderResponse<T> {
-	
-    private final List<T> content;
-    private final int page;
-    private final int size;
-    private final long totalElements;
-    private final int totalPages;
-    private final boolean last;
+import lombok.Getter;
 
-    public PageHolderResponse(Page<T> page) {
-        this.content = page.getContent();
-        this.page = page.getNumber();
-        this.size = page.getSize();
-        this.totalElements = page.getTotalElements();
-        this.totalPages = page.getTotalPages();
-        this.last = page.isLast();
-    }
+@Getter
+public class PageHolderResponse<T> {
+
+	private final List<T> content;
+	private final int page;
+	private final int size;
+	private final long totalElements;
+	private final int totalPages;
+	private final boolean last;
+
+	public PageHolderResponse(Page<T> page) {
+		this.content = page.getContent();
+		this.page = page.getNumber();
+		this.size = page.getSize();
+		this.totalElements = page.getTotalElements();
+		this.totalPages = page.getTotalPages();
+		this.last = page.isLast();
+	}
 
 }
-
