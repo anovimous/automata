d1c3ae9c276bdcac18c17da861da92add63f9206
anovimous <anovimous@gmail.com>
2026-04-25 21:35:28 +0300

    refactor(request): updated references to ContentType to match its new name RequestContentType

--- code/automata/src/main/java/com/automata/request/common/dto/RequestResponse.java
+++ code/automata/src/main/java/com/automata/request/common/dto/RequestResponse.java
@@ -1,6 +1,6 @@
 package com.automata.request.common.dto;
 
-import com.automata.request.common.enums.ContentType;
+import com.automata.request.common.enums.RequestContentType;
 import com.automata.request.common.enums.Method;
 import com.automata.request.common.enums.Source;
 
@@ -8,7 +8,7 @@ import lombok.Builder;
 
 @Builder
 public record RequestResponse(Method method, String computatedPath, String extension, String version,
-		int numberOfProperties, ContentType contentType, Source source, Long hostId, Long tenantId, Long programId,
-		Long responseId) {
+		int numberOfProperties, RequestContentType contentType, Source source, Long hostId, Long tenantId,
+		Long programId, Long responseId) {
 
 }
