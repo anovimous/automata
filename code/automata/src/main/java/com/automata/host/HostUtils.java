ce1136e61478c6bb527efcf14079fb0f5ca57982
anovimous <anovimous@gmail.com>
2026-05-11 19:35:52 +0300

    fix: fixed identifyLevel method to return the correct length of host

--- code/automata/src/main/java/com/automata/host/HostUtils.java
+++ code/automata/src/main/java/com/automata/host/HostUtils.java
@@ -8,16 +8,17 @@ import com.automata.common.utils.ValidationResult;
 public abstract class HostUtils {
 
 	public static int identifyLevel(String host) {
-		return host.split("\\.").length;
+		return host.split("\\.").length - 2;
 
 	}
 
 	public static ValidationResult validateHostFormat(String host) {
 		InetAddressValidator ipValidator = InetAddressValidator.getInstance();
 		DomainValidator domainValidator = DomainValidator.getInstance(true); // allow TLDs
-		return ipValidator.isValid(host) || domainValidator.isValid(host) ? ValidationResult.valid() : ValidationResult.invalid();
+		return ipValidator.isValid(host) || domainValidator.isValid(host) ? ValidationResult.valid()
+				: ValidationResult.invalid();
 	}
-	
+
 	public static ValidationResult validateRateLimits(int hostLimit, int longLimit, int shortLimit) {
 		return hostLimit >= (longLimit + shortLimit) ? ValidationResult.valid() : ValidationResult.invalid();
 	}
