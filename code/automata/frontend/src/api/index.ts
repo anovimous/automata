677d00eb53b95a9857dd33894c6b73c6f0c6fc8f
anovimous <anovimous@gmail.com>
2026-05-13 22:38:45 +0300

    refactor: removed frontend from project

--- code/automata/frontend/src/api/index.ts
+++
@@ -1,9 +0,0 @@
-export { programsApi } from "./programs";
-export { hostsApi } from "./hosts";
-export { tenantsApi } from "./tenants";
-export { requestsApi } from "./requests";
-export { responsesApi } from "./responses";
-export { jobsApi } from "./jobs";
-export { routinesApi } from "./routines";
-export { vulnerabilitiesApi } from "./vulnerabilities";
-export { comparatorsApi, modifiersApi, wordlistsApi } from "./other";
--- code/automata/frontend/src/routes/jobs/run-global.tsx
+++
@@ -1,5 +0,0 @@
-import { RunJobForm } from "@/routes/hosts/run-job";
-
-export function GlobalRunJobRoute() {
-  return <RunJobForm mode="global" search={{}} />;
-}
